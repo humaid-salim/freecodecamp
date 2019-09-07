ee6697d6978fd84828343a6cd7225b8520a9aa73
humaid-salim <humaidalbaddaai@gmail.com>
2019-09-07 23:30:32 +0400

    semi-ready

--- frontend/random_quote/jquery-file.js
+++ frontend/random_quote/jquery-file.js
@@ -1,41 +1,62 @@
-// Simple array of Objects.
-// every element in the array is
-// and object with two properties:
-//   "saying" which the Quote
-//   "sayer" which is the auther
-const sayings = [
-  {"saying":"hello world", "sayer": "every coder"},
-  {"saying":"get up", "sayer": "dad"},
-  {"saying":"where have you been?", "sayer": "my bros"}
-]
+// the array of objects url
+// every element is an object
+// witch contains two properties
+// name, and author.
 
-// ???
-const quoteObj = (arr) => {
+let url = "https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520"
+
+var sayings;
+const getSay = () => {
+  return $.ajax({
+    type: 'GET',
+    url: url,
+    async: false,
+    success: function(json) {
+      sayings = JSON.parse(json);
+    }
+  })
+}
+getSay();
+console.log(sayings);
+
+
+// Get one quote and its author from
+// array of objects.
+const quoteObj = function(arr) {
   let index = Math.floor(Math.random() * arr.length)
   return arr[index];
 }
 
-// ???
+var obj;
+// Bring a new quote.
 const setQuote = () => {
-  let obj = quoteObj(sayings);
+  obj = quoteObj(sayings);
   $("#author").fadeOut(function() {
-    $(this).text(obj.sayer);
+    $(this).text(obj.name);
     $(this).fadeIn();
   })
 
   $("#text").fadeOut( function() {
-    $(this).text(obj.saying);
+    $(this).text(obj.quote);
     $(this).fadeIn();
   });
 
 }
+
+const tweetRef = () => {
+  $('#tweet-quote').attr('href',
+  'https://twitter.com/intent/tweet?text='+ '"' + obj.quote + '" ' + obj.name);
+}
+
 
 // when the document is ready
 // I can click the new Quote
 // button.
 $(document).ready(function(){
   setQuote();
+  tweetRef();
   $("#new-quote").click(function () {
     setQuote();
+    tweetRef();
   });
 });
